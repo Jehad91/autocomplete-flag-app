fd4c0e9b01b0f7c62de5dc2e99d72c67ffd50b9b
Jehad Abushaqra <j.abushaqra1@gmail.com>
2022-02-23 18:28:58 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,8 +3,7 @@ const router = require('./router');
 
 const server = http.createServer(router);
 const port = process.env.PORT || 7000;
-const host = process.env.HOST || 'localhost';
 server.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server is running on http://${host}:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
